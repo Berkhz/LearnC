8a97bd0216130681c21a585eb01f394116865333
Kauan Bertalha <kauan.hb2004@gmail.com>
2023-02-09 17:36:29 -0300

    main

---
+++ algoritmos2/main/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    char nome; //caracter nome;
+    int idade; //inteiro idade;
+    float distanciaTerra; //ponto flutuante distanciaTerra;
+    double salario; //real salario;
+    bool lampada; //logico lampada;
+
+    printf("Olá, Seja bem vindo ao nosso sistema! \n");
+    printf("Como posso te chamar? \n");
+    scanf("%d \n", &nome);
+
+    system("pause");
+    return 0;
+}
