a53f64bafb372887be64735a1eba9d510b8221a2
Kauan Bertalha <kauan.hb2004@gmail.com>
2023-02-10 00:48:07 -0300

    update

--- algoritmos2/main/main.c
+++ algoritmos2/main/main.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 
 int main(void){
-    char nome; //caracter nome;
-    int idade; //inteiro idade;
-    float distanciaTerra; //ponto flutuante distanciaTerra;
-    double salario; //real salario;
-    bool lampada; //logico lampada;
+    // char nome; //caracter nome;
+    // int idade; //inteiro idade;
+    // float distanciaTerra; //ponto flutuante distanciaTerra;
+    // double salario; //real salario;
+    // bool lampada; //logico lampada;
 
     printf("Olá, Seja bem vindo ao nosso sistema! \n");
-    printf("Como posso te chamar? \n");
-    scanf("%d \n", &nome);
-    fflush(stdin);
+    // printf("Como posso te chamar? \n");
+    // scanf("%d \n", &nome);
+    // fflush(stdin);
 
     system("pause");
     return 0;
