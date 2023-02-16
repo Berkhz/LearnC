e374fb325829e069fa672ad661beee7e789eca0a
Kauan Bertalha <kauan.hb2004@gmail.com>
2023-02-16 00:15:52 -0300

    Calculadora receita bolo

--- algoritmos2/boloCenoura/main.c
+++ algoritmos2/boloCenoura/main.c
@@ -4,7 +4,9 @@
 
 int main() {
     setlocale(LC_ALL, "portuguese");
-    float oleo_soja, cenoura, ovos, acucar_refinado, farinha_trigo, fermento_po, chocolate_po, manteiga;
+    float etapa_1, etapa_2, oleo_soja, cenoura, 
+    ovos, acucar_refinado,  farinha_trigo, 
+    fermento_po, chocolate_po, manteiga;
 
     printf("Preço do óleo de soja? \n");
     scanf("%f, \n", &oleo_soja);
@@ -38,6 +40,15 @@ int main() {
     scanf("%f, \n", &manteiga);
     fflush(stdin);
 
+    etapa_1 = ((oleo_soja/1000)*120)  + ((cenoura/7)*3) + ((ovos/12)*4) + ((acucar_refinado/1000)*320) +
+    ((farinha_trigo/1000)*300) + ((fermento_po/50)*25);
+
+    etapa_2 = ((manteiga/500)*12) + ((chocolate_po/200)*18) + ((acucar_refinado/1000)*150);
+
+    printf("O cálculo do bolo foi de: %.2f \n", etapa_1);
+    printf("O cálculo da cobertura do bolo foi de: %.2f \n", etapa_2);
+    printf("O gasto total foi de: %.2f \n", (etapa_1 + etapa_2));
+    
     system("pause");
     return 0;
 }
