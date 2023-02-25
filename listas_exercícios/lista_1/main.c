99966e1062430772694649eaae9b3f82bc0460aa
Kauan Bertalha <kauan.hb2004@gmail.com>
2023-02-24 21:29:18 -0300

    Lista atualizada

--- listas_exercícios/lista_1/main.c
+++ listas_exercícios/lista_1/main.c
@@ -52,7 +52,7 @@ int main(void) {
 
         case 6:
         //6) Ler uma variável e apresentar 25% da mesma.
-        printf("Insira um valor que vou te mostrar 25%, deste valor:\n");
+        printf("Insira um valor que vou te mostrar 25%% deste valor:\n");
         scanf("%f", &num1Decimal);
         fflush(stdin);
         num1Decimal = num1Decimal * 0.25;
@@ -61,7 +61,7 @@ int main(void) {
         
         case 7:
         //7) Ler uma variável e apresentar 60% da mesma.
-        printf("Insira um valor que vou te mostrar 60%, deste valor:\n");
+        printf("Insira um valor que vou te mostrar 60%% deste valor:\n");
         scanf("%f", &num1Decimal);
         fflush(stdin);
         num1Decimal = num1Decimal * 0.60;
@@ -70,7 +70,7 @@ int main(void) {
 
         case 8:
         //8) Ler duas variáveis a apresentar 45% da soma destas.
-        printf("Insira dois valores que vou te mostrar 45%, da soma destas:\nPrimeiro valor:");
+        printf("Insira dois valores que vou te mostrar 45%% da soma destas:\nPrimeiro valor:");
         scanf("%f", &num1Decimal);
         fflush(stdin);
         printf("Segundo valor:");
@@ -121,6 +121,94 @@ int main(void) {
         resultadoDecimal = (num1Decimal + num2Decimal * 2) / num3Decimal;
         printf("Resultado da fórmula Z = (L + A * 2) / C\nZ = %.2f\n", resultadoDecimal);
         break;
+
+        case 12:
+        //12) Ler uma variável e acumular em outra um aumento de 25%, apresentado-a no final.
+        printf("Insira um valor:");
+        scanf("%f", &num1Decimal);
+        resultadoDecimal = num1Decimal * 1.25;
+        printf("Resultado com aumento de 25%% = %.2f\n", resultadoDecimal);
+        break;
+
+        case 13:
+        //13) Ler duas variáveis numérica, e apresentar a soma das mesmas, acumulando ou não em outra variável.
+        printf("Insira dois números inteiros!\nPrimeiro valor:");
+        scanf("%i", &num1);
+        printf("Segundo valor:");
+        scanf("%i", &num2);
+        printf("Resultado = %i\n", num1 + num2);
+        break;
+
+        case 14:
+        //14) AREA = (BASE x ALTURA) / 2. Área de um Triângulo
+        printf("Vamos descobrir a área de um triângulo!\nValor da Base:");
+        scanf("%f", &num1Decimal);
+        printf("Valor altura:");
+        scanf("%f", &num2Decimal);
+        resultadoDecimal = (num1Decimal * num2Decimal) / 2;
+        printf("Valor da ÁREA = %.2f\n", resultadoDecimal);
+        break;
+
+        case 15:
+        //15) CAC = 3.1416 * R². Lembre-se que R² é o mesmo que (R * R). Área de um Círculo
+        printf("Vamos descobrir a área de um círculo!\nValor do raio:");
+        scanf("%f", &num1Decimal);
+        resultadoDecimal = 3.1416 * (num1Decimal * num1Decimal);
+        printf("Valor da ÁREA do CÍRCULO = %.2f\n", resultadoDecimal);
+        break;
+
+        case 16:
+        //16) CPC = 2* 3.1416 * R Perímetro de Círculo
+        printf("Vamos descobrir o perímetro de um círculo!\nValor do raio:");
+        scanf("%f", &num1Decimal);
+        resultadoDecimal = (2 *(3.1416 * num1Decimal));
+        printf("Valor do perímetro do CÍRCULO = %.2f\n", resultadoDecimal);
+        break;
+
+        case 17:
+        //17) CVC = (3.1416 * R²) * H Volume de Círculo
+        printf("Vamos descobrir o volume de um círculo!\nValor do raio:");
+        scanf("%f", &num1Decimal);
+        printf("Altura:");
+        scanf("%f", &num2Decimal);
+        resultadoDecimal = (3.1416 * (num1Decimal * num1Decimal)) * num2Decimal;
+        printf("Valor do volume de um círculo = %.2f\n", resultadoDecimal);
+        break;
+
+        case 18:
+        //18) VCN = ((3.1416 * R²) / 3) * H Volume de um Cone
+        printf("Vamos descobrir o volume de um cone!\nValor do raio:");
+        scanf("%f", &num1Decimal);
+        printf("Altura:");
+        scanf("%f", &num2Decimal);
+        resultadoDecimal = ((3.1416 * (num1Decimal * num1Decimal))/3) * num2Decimal;
+        printf("Valor do volume do cone = %.2f\n", resultadoDecimal);
+        break;
+
+        case 19:
+        //19) VPD = (L * L) * (H / 3) Volume de Pirâmide
+        printf("Vamos descobrir o volume de uma pirâmide!\nValor dos lados:");
+        scanf("%f", &num1Decimal);
+        printf("Altura:");
+        scanf("%f", &num2Decimal);
+        resultadoDecimal = (num1Decimal * num1Decimal) * (num2Decimal / 3);
+        printf("Valor do volume da pirâmide = %.2f\n", resultadoDecimal);
+        break;
+
+        case 20:
+        //20) VPM = L1 * L2 * L3 Volume de Prisma
+        printf("Vamos descobrir o volume de um prisma!\nValor do lado 1:");
+        scanf("%f", &num1Decimal);
+        printf("Vamos descobrir o volume de um prisma!\nValor do lado 2:");
+        scanf("%f", &num2Decimal);
+        printf("Vamos descobrir o volume de um prisma!\nValor do lado 3:");
+        scanf("%f", &num3Decimal);
+        resultadoDecimal = (num1Decimal * num2Decimal * num3Decimal);
+        printf("Valor do volume de um prisma = %.2f\n", resultadoDecimal);
+        break;
+
+        default:
+        printf("Tente novamente!\n");
     }
     system("pause");
     return 0;
