ef38561f2d64c9b6c589d1c98d8dbf656017969e
Kauan Bertalha <kauan.hb2004@gmail.com>
2023-03-24 12:52:11 -0300

    Lista 3 atualizada

--- listas_exercícios/lista_3/main.c
+++ listas_exercícios/lista_3/main.c
@@ -4,8 +4,9 @@
 
 int main(void) {
     setlocale(LC_ALL, "Portuguese");
-    char nome[50], sexo[1];
+    char nome[50], sexo;
     int idade, escolhaExercicio, i;
+    float num1Decimal, auxDecimal;
 
     printf("Essa é a lista 3 de exercícios!\nEscolha um exercício de 1 a 15\n");
     scanf("%i", &escolhaExercicio);
@@ -75,6 +76,79 @@ int main(void) {
         {
             printf("%i ", i);
         }
+        break;
+
+        case 6:
+        //6) Solicite o nome, idade e sexo de uma pessoa. Valide o sexo entre (“F” / “M”) idade entre 10 e 100 anos
+        printf("Informe seu nome: ");
+        scanf("%s", &nome);
+        fflush(stdin);
+
+        do 
+        {
+            printf("Sua idade: ");
+            scanf("%i", &idade);
+            fflush(stdin);
+        }
+        while (idade < 10 || idade > 100);
+
+        do 
+        {
+            printf("Seu sexo: (F / M): ");
+            scanf("%c", &sexo);
+            fflush(stdin);
+        }
+        while (sexo != 'F' && sexo != 'M');
+
+        printf("Seu nome eh %s ", nome);
+        printf("voce tem %i anos ", idade);
+        printf("e possui o sexo %c! \n", sexo);
+        break;
+
+        case 7:
+        //7) Solicite 15 número (entre 10 e 50) e apresente posteriormente a média
+        for (i = 1; i <= 15; i++)
+        {
+            do
+            {
+                printf("Informe %i numero: ", i);
+                scanf("%f", &num1Decimal);
+                fflush(stdin);
+            }
+            while (num1Decimal < 10 || num1Decimal > 50);
+            auxDecimal += num1Decimal;
+        }
+        printf("A media eh de: %.2f \n", auxDecimal / 15);
+        break;
+
+        case 8:
+        //8) Apresente todos os números pares entre 5 e 50 (um na frente do outro) – Utilize o DO WHILE
+        i = 5;
+        do
+        {
+            if (i % 2 == 0)
+            {
+                printf("%i ", i);
+            }
+            i++;
+        }
+        while (i <= 50);
+        break;
+
+        case 9:
+        //9) Apresente todos os números ímpares entre 5 e 50 (um na frente do outro) – Utilize o FOR
+        for (i = 5; i <= 50; i++)
+        {
+            if (i % 2 == 1)
+            {
+                printf("%i ", i);
+            }
+        }
+        break;
+
+        case 10:
+        //10) Solicite 10 números ao usuário. A cada número solicitado, apresente a mensagem PAR ou IMPAR e no fim,
+        //apresente a média dos números ímpares.
         break;
         
     default:
